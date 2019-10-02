a9be2390625a25d8740c8b8c31bd163cb6cb83d7
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-10-03 04:36:27 +0900

    Fix TypeScript compile error

--- plugins/mock.ts
+++ plugins/mock.ts
@@ -1,5 +1,7 @@
 import { Context } from '@nuxt/types'
 import mockServer from 'axios-mock-server'
+
+// @ts-ignore: Cannot find module '~/mocks/$route'
 import route from '~/mocks/$route'
 
 export default ({ $axios }: Context) => {
