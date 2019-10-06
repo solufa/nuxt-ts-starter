3eb83eec279005cb913dd80468a332b28c8f7884
m-mitsuhide <genbu2011@live.jp>
2019-10-07 05:22:27 +0900

    feat(mock): update version

--- plugins/mock.ts
+++ plugins/mock.ts
@@ -1,9 +1,5 @@
 import { Context } from '@nuxt/types'
-import mockServer from 'axios-mock-server'
-
 // @ts-ignore: Cannot find module '~/mocks/$route'
-import route from '~/mocks/$route'
+import mock from '~/mocks/$mock'
 
-export default ({ $axios }: Context) => {
-  mockServer(route, $axios).enableLog()
-}
+export default ({ $axios }: Context) => mock($axios).enableLog()
