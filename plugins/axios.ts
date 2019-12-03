d42f6282b7a12ccfc2ad853ab645caa555805a16
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-12-03 10:27:19 +0900

    fix(lint): add comment to disable no-console error

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,6 +2,7 @@ import { Context } from '@nuxt/types'
 
 export default ({ $axios }: Context) => {
   $axios.onError((error) => {
+    // eslint-disable-next-line no-console
     console.log(error)
   })
 }
