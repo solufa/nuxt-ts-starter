0ed2fe5c73e9962c32da61f6f99ebbf6568e80cb
solufa <solufa2020@gmail.com>
2021-08-12 09:32:09 +0900

    feat: support typed-css-modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config'
 import { NuxtConfig } from '@nuxt/types'
+import 'dotenv/config'
 
 const {
   npm_package_name: TITLE,
   npm_package_description: DESCRIPTION,
-  ENABLE_MOCK
+  ENABLE_MOCK,
 } = process.env
 
 const config: NuxtConfig = {
@@ -19,26 +19,22 @@ const config: NuxtConfig = {
   head: {
     title: TITLE || '',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: DESCRIPTION || '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/styles/settings.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    ENABLE_MOCK === 'true' ? '~/plugins/mock' : '~/plugins/api'
-  ],
+  plugins: [ENABLE_MOCK === 'true' ? '~/plugins/mock' : '~/plugins/api'],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -53,19 +49,28 @@ const config: NuxtConfig = {
   vite: {
     vue: {
       jsx: true,
-      jsxOptions: { compositionAPI: true }
-    }
+      jsxOptions: { compositionAPI: true },
+    },
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    loaders: {
+      css: {
+        modules: {
+          // @ts-expect-error
+          mode: (filePath: string) =>
+            /\.module\.css$/.test(filePath) ? 'local' : 'global',
+        },
+      },
+    },
+  },
 }
 
 export default config
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 import Tutorial from '~/components/Tutorial.vue'
+import styles from './styles.module.css'
 
 export default defineComponent({
   setup() {
-    return () => <Tutorial />
+    return () => (
+      <div class={styles.sampleFont}>
+        <Tutorial />
+      </div>
+    )
   },
 })
